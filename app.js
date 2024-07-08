b2aedf76b46f221985ef0fa9cb3df24f602e6496
Tuguldur <tuugii24758@gmail.com>
2024-07-08 14:17:36 +0800

    Orlogo zarlagig delgetsen deer gargana

--- app.js
+++ app.js
@@ -1,22 +1,132 @@
 // Delgetstei ajillah controller
-var uiControler = (function () {})();
+var uiControler = (function () {
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    addBtn: ".add__btn",
+  };
+
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, // exp inc
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
+
+    getDOMStrings: function () {
+      return DOMstrings;
+    },
+
+    addListItem: function (item, type) {
+      // Orlogo zarlagin elementiig aguulsn html beldene.
+      var html, list;
+      if (type === "inc") {
+        list = ".income__list";
+        html =
+          '<div class="item clearfix" id="income-%id%"><div class="item__description">%DESCRIPTION%</div><div class="right clearfix"><div class="item__value">+ %Value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      } else {
+        list = ".expenses__list";
+        html =
+          '<div class="item clearfix" id="expense-%id%"><div class="item__description">%DESCRIPTION%</div><div class="right clearfix"><div class="item__value">- %Value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      }
+      // Ter html dotroo orlogo zarlagin utguudiig Replace ashiglaj oorclono
+      html = html.replace("%id%", item.id);
+      html = html.replace("%DESCRIPTION%", item.description);
+      html = html.replace("%Value%", item.value);
+      // Beltgesen html ee dom ruu hiij ogno.
+      document.querySelector(list).insertAdjacentHTML("beforeend", html);
+    },
+  };
+})();
 
 // Sanhuutei ajillah controller
-var financeControler = (function () {})();
+// Private Data
+var financeControler = (function () {
+  var Income = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var Expense = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var data = {
+    items: {
+      inc: [],
+      exp: [],
+    },
+
+    totals: {
+      inc: 1000,
+      exp: 500,
+    },
+  };
+
+  return {
+    addItem: function (type, desc, val) {
+      var item, id;
+
+      if (data.items[type].length === 0) id = 1;
+      else {
+        id = data.items[type][data.items[type].length - 1].id + 1;
+      }
+
+      if (type === "inc") {
+        item = new Income(id, desc, val);
+      } else {
+        item = new Expense(id, desc, val);
+      }
+      data.items[type].push(item);
+
+      return item;
+    },
+
+    seeData: function () {
+      return data;
+    },
+  };
+})();
 
 // Programmiin holbogc controller
 var appControler = (function (uiCtrl, fnCtrl) {
   var ctrlAddItem = function () {
-    console.log("delgetsees ogogdloo awah heseg");
+    var input = uiControler.getInput();
+
+    var item = financeControler.addItem(
+      input.type,
+      input.description,
+      input.value
+    );
+
+    uiControler.addListItem(item, input.type);
   };
 
-  document.querySelector(".add__btn").addEventListener("click", function () {
-    ctrlAddItem();
-  });
-
-  document.addEventListener("keypress", function (event) {
-    if (event.keyCode === 13) {
+  var setupEventListeners = function () {
+    var DOM = uiControler.getDOMStrings();
+    document.querySelector(DOM.addBtn).addEventListener("click", function () {
       ctrlAddItem();
-    }
-  });
+    });
+
+    document.addEventListener("keypress", function (event) {
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+
+  return {
+    init: function () {
+      console.log("Application started...");
+      setupEventListeners();
+    },
+  };
 })(uiControler, financeControler);
+
+appControler.init();
